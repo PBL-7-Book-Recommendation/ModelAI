244a74668ead1dc0947a906abecb095f2469c2d1
Marvellous-bhp <bhphuocforwork@gmail.com>
2024-06-06 17:07:41 +0700

    update requirement flask

--- src/app.py
+++ src/app.py
@@ -5,8 +5,7 @@ import os
 
 app = Flask(__name__)
 pathlib.PosixPath = pathlib.WindowsPath
-path_to_model = os.path.join(os.path.dirname(os.getcwd()), 'src/models/all_ratings_fit_30_5.pkl')
-learn = load_learner_from_path(path_to_model)
+
 
 @app.route('/recommend_book/<userID>', methods=['GET'])
 def recommend_books_for_user(userID):
