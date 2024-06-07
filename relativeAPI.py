5441fe1d72d3a7037e3191f703c3c9dd9aff4dd6
Marvellous-bhp <bhphuocforwork@gmail.com>
2024-06-07 22:30:15 +0700

    const port

--- relativeAPI.py
+++ relativeAPI.py
@@ -7,6 +7,7 @@ from src.helper_CB import *
 from src.helper_query import *
 import pathlib
 
+
 app = Flask(__name__)
 # Apply Flask CORS
 CORS(app)
@@ -24,12 +25,14 @@ path_to_model_CF = os.path.join(os.path.dirname(os.getcwd()), './src/models/all_
 
 learn = load_learner_from_path(path_to_model_CF)
 
-# Default API
+# defaul api
 @app.route('/', methods=['GET'])
 def default_router():
-    return 'hello'
+    a = 'hello'
+    return a
 
-# API to return book list based on book ID
+
+# API để trả về danh sách sách dựa trên danh sách id
 @app.route("/content-based-recommend/<string:book_id>", methods=["GET"])
 @cross_origin(origin="*")  # Fix to current web domain
 def get_books(book_id):
@@ -43,19 +46,23 @@ def get_books(book_id):
 
     return jsonify({"data": recommend_books_CB})
 
+
 @app.route('/recommend_CF_book/<userID>', methods=['GET'])
-@cross_origin(origin="*")
+@cross_origin(origin="*") 
 def recommend_books_for_user(userID):
     unrated_books_info = get_book_unrate(userID)
     index_list_book = get_index_book_recommend(unrated_books_info)
     book_weights = get_weights(learn, index_list_book)
     recommend_books = recommend_books_list(book_weights, unrated_books_info)
-    print(recommend_books, 'recom cf')
+    print(recommend_books,'recom cf')
     recommend_books_CF = get_books_by_ids(recommend_books)
 
     return jsonify({"data": recommend_books_CF})
 
+    # return jsonify({"recommend_books": recommend_books})
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable if available
-    print(port, 'port')
-    app.run(host="0.0.0.0", port=port, debug=True)
+
+    # port = 5000
+    # print(port, 'port')
+    app.run(host="0.0.0.0", port=3000, debug=True)
